f10059492f732d76a87dd25f50fb6cbdc57a5886
Fernando de Faria <nandodefaria@yahoo.com>
2020-12-29 00:51:28 -0300

    Criacao do tabuleiro

--- xadrez/Program.cs
+++ xadrez/Program.cs
@@ -7,9 +7,7 @@ namespace xadrez
     {
         static void Main(string[] args)
         {
-            Posicao P;
-            P = new Posicao(3, 4);
-            Console.WriteLine(P);
+            Tabuleiro tab = new Tabuleiro(8, 8);
             Console.ReadLine();
         }
     }
---
+++ xadrez/tabuleiro/Peca.cs
@@ -0,0 +1,19 @@
+﻿namespace tabuleiro
+{
+    class Peca
+    {
+        public Posicao posicao {get; set; }
+        public Cor cor { get; private set; }
+        public int qntMovimentos { get; private set; }
+
+        public Tabuleiro tab { get; private set; }
+
+        public Peca(Posicao posicao, Tabuleiro tab, Cor cor)
+        {
+            this.posicao = posicao;
+            this.tab = tab;
+            this.cor = cor;
+            this.qntMovimentos = 0;
+        }
+    }
+}
