3057a68efab8120483e8ad7ef669319bc1f6172e
Fernando de Faria <nandodefaria@yahoo.com>
2020-12-30 00:59:29 -0300

    Erros com excecao personalizada

--- xadrez/tabuleiro/Tabuleiro.cs
+++ xadrez/tabuleiro/Tabuleiro.cs
@@ -11,15 +11,45 @@
             this.linhas = linhas;
             this.colunas = colunas;
             pecas = new Peca[linhas, colunas];
+            System.Console.WriteLine();
         }
         public Peca peca(int linha, int coluna)
         {
             return pecas[linha, coluna];
         }
+        public Peca peca(Posicao pos)
+        {
+            return pecas[pos.linha, pos.coluna];
+        }
+        public bool existePeca(Posicao pos)
+        {
+            validarPosicao(pos);
+            return peca(pos) != null;
+        }
         public void colocarPeca(Peca p, Posicao pos)
         {
+            if (existePeca(pos))
+            {
+                throw new TabuleiroException("Já existe uma peça na posição nesta posição!");
+            }
             pecas[pos.linha, pos.coluna] = p;
             p.posicao = pos;
         }
+        public bool posicaoValida(Posicao pos)
+        {
+            if (pos.linha < 0 || pos.linha >= linhas || pos.coluna < 0 || pos.coluna >= colunas)
+            {
+                return false;
+            }
+            return true;
+        }
+        public void validarPosicao(Posicao pos)
+        {
+            if (!posicaoValida(pos))
+            {
+                throw new TabuleiroException("Posição Invalida!");
+            }
+        }
+
     }
 }
