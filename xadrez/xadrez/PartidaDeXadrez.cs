26171deb327b1f6589c41d99c15695656d5f1b6f
Fernando de Faria <nandodefaria@yahoo.com>
2021-01-04 02:21:29 -0300

    Jogada especial promocao

--- xadrez/xadrez/PartidaDeXadrez.cs
+++ xadrez/xadrez/PartidaDeXadrez.cs
@@ -138,6 +138,22 @@ namespace xadrez
                 desfazerMovimento(origem, destino, pecaCapturada);
                 throw new TabuleiroException("Você não pode se colocar em xeque ou deve sair dele! Enter´para continuar...");
             }
+
+            Peca p = tab.peca(destino);
+
+            // #jogada especial promocao
+
+            if(p is Peao)
+            {
+                if((p.cor ==Cor.Branca && destino.linha == 0 ) || (p.cor == Cor.Preta && destino.linha == 7))
+                {
+                    p = tab.retirarPeca(destino);
+                    pecas.Remove(p);
+                    Peca dama = new Dama(tab, p.cor);
+                    tab.colocarPeca(dama, destino);
+                    pecas.Add(dama);
+                }
+            }
             if (estaEmXeque(adversaria(jogadorAtual)))
             {
                 xeque = true;
@@ -157,7 +173,7 @@ namespace xadrez
             }
 
             // #jogada especial en passant
-            Peca p = tab.peca(destino);
+            
             if(p is Peao && (destino.linha == origem.linha +2 || destino.linha == origem.linha - 2))
             {
                 vulneravelEnPassant = p;
